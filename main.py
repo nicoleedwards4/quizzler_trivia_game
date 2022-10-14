93ca5a5f21009cdbb7fd975e0e6581aebfaa57da
Nicole Edwards <nicolejedwards5@gmail.com>
2022-10-13 20:57:49 -0700

    added main

---
+++ main.py
@@ -0,0 +1,27 @@
+from question_model import Question
+from quiz_brain import QuizBrain
+import requests
+from ui import QuizInterface
+
+question_bank = []
+
+response = requests.get(url="https://opentdb.com/api.php?amount=10&type=boolean")
+response.raise_for_status()
+question_data = response.json()
+question_data = question_data["results"]
+
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+
+quiz = QuizBrain(question_bank)
+quiz_ui = QuizInterface(quiz)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
